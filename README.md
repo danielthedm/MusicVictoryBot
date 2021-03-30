62581fbb7f1dfc223dccd6293b6c9803b397063e
dles91 <lesliede91@gmail.com>
2021-03-30 13:40:07 -0400

    inital commit

---
+++ README.md
@@ -0,0 +1,6 @@
+Steps to run
+
+1. Clone repo
+2. Run `npm install`
+3. Add Discord credentials/channelId in a `config.json` file
+3. Run `node index.js`
---
+++ index.js
@@ -0,0 +1,114 @@
+const Discord = require("discord.js");
+const { prefix, token, channelId } = require("./config.json");
+const ytdl = require("ytdl-core");
+
+const client = new Discord.Client();
+
+const queue = new Map();
+
+client.once("ready", () => {
+  console.log("Ready!");
+});
+
+client.once("reconnecting", () => {
+  console.log("Reconnecting!");
+});
+
+client.once("disconnect", () => {
+  console.log("Disconnect!");
+});
+
+client.on("message", async message => {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+
+  const serverQueue = queue.get(message.guild.id);
+
+  if (message.content.startsWith(`${prefix}chickendinner`)) {
+    execute(message, serverQueue);
+    return;
+  } else {
+    message.channel.send("You need to enter a valid command!");
+  }
+});
+
+async function execute(message, serverQueue) {
+  const args = message.content.split(" ");
+
+  console.log(message)
+
+  if(!message.channel.id == channelId){
+    return
+  }
+
+
+  const voiceChannelID = message.member.voiceChannelID;
+  const voiceChannel = message.member.guild.channels.get(voiceChannelID)
+  if (!voiceChannel)
+    return message.channel.send(
+      "You need to be in a voice channel to play music!"
+    );
+  const permissions = voiceChannel.permissionsFor(message.client.user);
+  if (!permissions.has("CONNECT") || !permissions.has("SPEAK")) {
+    return message.channel.send(
+      "I need the permissions to join and speak in your voice channel!"
+    );
+  }
+
+  const song = {
+      title: 'Creedence Clearwater Revival - Fortunate Son (Official Lyric Video)',
+      url: 'https://www.youtube.com/watch?v=40JmEj0_aVM'
+    };
+
+  if (!serverQueue) {
+    const queueContruct = {
+      textChannel: message.channel,
+      voiceChannel: voiceChannel,
+      connection: null,
+      songs: [],
+      volume: 5,
+      playing: true
+    };
+
+    queue.set(message.guild.id, queueContruct);
+
+    queueContruct.songs.push(song);
+
+    try {
+      var connection = await voiceChannel.join();
+      queueContruct.connection = connection;
+      play(message.guild, queueContruct.songs[0]);
+    } catch (err) {
+      console.log(err);
+      queue.delete(message.guild.id);
+      return message.channel.send(err);
+    }
+  } else {
+    serverQueue.songs.push(song);
+  }
+}
+
+function play(guild, song) {
+  const serverQueue = queue.get(guild.id);
+  if (!song) {
+    serverQueue.voiceChannel.leave();
+    queue.delete(guild.id);
+    return;
+  }
+
+  const dispatcher = serverQueue.connection
+    .playStream(ytdl(song.url), {filter: 
+      "audioonly"})
+    .on("end", () => {
+      serverQueue.songs = [];
+      play(guild, serverQueue.songs[0]);
+    })
+    .on("finish", () => {
+      serverQueue.songs = [];
+      play(guild, serverQueue.songs[0]);
+    })
+    .on("error", error => console.error(error));
+  dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);
+}
+
+client.login(token);
